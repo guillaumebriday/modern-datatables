74326c5b9dc7016ad93399dfbcdf9b80a9c6eb48
Guillaume Briday <guillaumebriday@gmail.com>
2020-12-27 12:07:25 +0100

    Cleaning controller and adding template

--- rails-stimulusreflex/app/javascript/controllers/index.js
+++ rails-stimulusreflex/app/javascript/controllers/index.js
@@ -2,12 +2,8 @@
 // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 
 const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-
-application.load(definitionsFromContext(context))
 StimulusReflex.initialize(application, { consumer })
