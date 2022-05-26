68485b9316a3a11e7cfc547676df07ea34c19c87
Guillaume Briday <guillaumebriday@gmail.com>
2022-05-26 21:47:08 +0200

    wip

--- rails-hotwire/config/importmap.rb
+++ rails-hotwire/config/importmap.rb
@@ -2,3 +2,6 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
