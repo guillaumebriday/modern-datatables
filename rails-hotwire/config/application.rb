d5cc3991d2ea7779cb650f262c6fb6cea921ca15
Guillaume Briday <guillaumebriday@gmail.com>
2023-11-09 00:51:05 -0500

    Upgrading Rails to 7.1

--- rails-hotwire/config/application.rb
+++ rails-hotwire/config/application.rb
@@ -14,7 +14,6 @@ require 'action_controller/railtie'
 # require "action_text/engine"
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,7 +23,12 @@ Bundler.require(*Rails.groups)
 module RailsHotwire
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
