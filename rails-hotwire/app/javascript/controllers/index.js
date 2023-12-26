cc1df89649eb1dd027a229fe8fcbc7e7048e3fbe
Guillaume Briday <guillaumebriday@gmail.com>
2023-12-25 20:41:07 -0500

    Adding stimulus-rails-autosave

--- rails-hotwire/app/javascript/controllers/index.js
+++ rails-hotwire/app/javascript/controllers/index.js
@@ -1,4 +1,8 @@
+import Autosave from 'stimulus-rails-autosave'
 import { application } from "controllers/application"
 
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+
+application.register('autosave', Autosave)
--- rails-hotwire/config/importmap.rb
+++ rails-hotwire/config/importmap.rb
@@ -7,3 +7,5 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'stimulus-rails-autosave', to: 'https://ga.jspm.io/npm:stimulus-rails-autosave@5.0.1/dist/stimulus-rails-autosave.mjs'
+pin 'lodash.debounce', to: 'https://ga.jspm.io/npm:lodash.debounce@4.0.8/index.js'
