79dd567f7328e62b5cde4eb8161c31cdde8f3fec
Guillaume Briday <guillaumebriday@gmail.com>
2024-09-29 02:25:34 -0400

    Cache assets in production

--- rails-hotwire/config/environments/production.rb
+++ rails-hotwire/config/environments/production.rb
@@ -23,7 +23,9 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{1.year.to_i}, immutable"
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
